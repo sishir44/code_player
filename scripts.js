03499fc80ef27eae82e6ed48e06b956b177f0774
Sishir Bohara <54624284+sishir44@users.noreply.github.com>
2022-11-22 10:45:12 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function updateOutput() {
+    $("iframe").contents().find("html").html("<html><head>" + $("#cssPanel").val() + "</head><body>" + $("#htmlPanel").val() + "</body></html>");
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+}
+
+$(".toggleButton").hover(function() {
+    $(this).addClass("highlightedButton");
+}, function() {
+    $(this).removeClass("highlightedButton");
+});
+
+$(".toggleButton").click(function() {
+    $(this).toggleClass("active");
+    $(this).removeClass("highlightedButton");
+
+    var panelId = $(this).attr("id") + "Panel";
+    $("#" + panelId).toggleClass("hidden");
+
+    var numberOfActivePanels = 4 - $('.hidden').length;
+    $(".panel").width(($(window).width() / numberOfActivePanels)- 10);
+});
+
+$(".panel").height($(window).height() - $("#header").height() - 15);
+$(".panel").width(($(window).width() / 2)- 10);
+
+updateOutput();
+
+$("textarea").on('change keyup paste', function() {
+    updateOutput();
+});
